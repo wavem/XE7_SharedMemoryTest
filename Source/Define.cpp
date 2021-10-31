b0315301817a4b03d7556e1f7db9003977342434
WAVE <fenix24@naver.com>
2021-10-31 18:24:40 +0900

    [New] Basic Shared Memory Class Design

--- Source/Define.cpp
+++ Source/Define.cpp
@@ -3,5 +3,47 @@
 #pragma hdrstop
 
 #include "Define.h"
+#include "Main.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+
+
+
+
+__fastcall CSharedMemory::CSharedMemory() {
+	m_handle = NULL;
+    m_strName = L"";
+    m_nMemSize = 0;
+    m_Ptr = NULL;
+}
+//---------------------------------------------------------------------------
+
+bool __fastcall CSharedMemory::Create() {
+	m_handle = OpenFileMapping(FILE_MAP_ALL_ACCESS, NULL, m_strName.c_str());
+    if (m_handle != NULL) {
+    	FormMain->PrintMsg(L"It is already existed.");
+        return true;
+    }
+
+    m_handle = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, m_nMemSize, m_strName.c_str());
+    if (m_handle == NULL) {
+    	FormMain->PrintMsg(L"Failed to create SM.");
+        return false;
+    }
+    return true;
+}
+//---------------------------------------------------------------------------
+
+bool __fastcall CSharedMemory::Attach() {
+	if (m_Ptr != NULL) {
+    	FormMain->PrintMsg(L"You already attached to SM.");
+        return false;
+    }
+    m_Ptr = MapViewOfFile(m_handle, FILE_MAP_ALL_ACCESS, 0, 0, m_nMemSize);
+    if (m_Ptr == NULL) {
+    	FormMain->PrintMsg(L"Failed to create SM.");
+        return false;
+    }
+    return true;
+}
+//---------------------------------------------------------------------------
--- Source/Define.h
+++ Source/Define.h
@@ -7,5 +7,32 @@
 #include <System.Classes.hpp>
 
 
+typedef struct st_Protocol {
+	BYTE Data;
+} Protocol;
+
+
+class CSharedMemory {
+private:
+
+public:
+	__fastcall CSharedMemory();
+    __fastcall ~CSharedMemory();
+
+// Member Variables
+	HANDLE m_handle;
+    UnicodeString m_strName;
+    __int64 m_nMemSize;
+    void* m_Ptr;
+
+
+// Member Functions
+	bool __fastcall Create();
+    bool __fastcall Attach();
+
+
+
+};
+
 //---------------------------------------------------------------------------
 #endif
